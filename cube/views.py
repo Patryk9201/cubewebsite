873cc0ba4cf7697b39f46e3fb3ee64afc570c117
patrykwozniak9201 <patrykwozniak9201@gmail.com>
2021-02-11 21:12:16 +0100

    Initial commit

---
+++ cube/views.py
@@ -0,0 +1,19 @@
+from django.views.generic import TemplateView
+from django.shortcuts import redirect
+
+
+
+
+
+
+class HomeTemplateView(TemplateView):
+    template_name = 'home.html'
+
+    # override get context date method
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+
+
+
+
